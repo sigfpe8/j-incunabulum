164309d6742c4ba7cd4d3282fd51fa7ac333448d
sigfpe8 <sigfpe8@gmail.com>
2024-06-07 12:20:30 -0300

    Fix pedantic errors and extra warnings

--- nj.c
+++ nj.c
@@ -20,18 +20,18 @@ V2(from){I r=w->r-1,*d=w->d+1,n=tr(r,d);
 V1(box){A z=ga(1,0,0);*z->p=(I)w;R z;}
 V2(cat){I an=tr(a->r,a->d),wn=tr(w->r,w->d),n=an+wn;
  A z=ga(w->t,1,&n);mv(z->p,a->p,an);mv(z->p+an,w->p,wn);R z;}
-V2(find){R 0;}
+//V2(find){R 0;}
 V2(rsh){I r=a->r?*a->d:1,n=tr(r,a->p),wn=tr(w->r,w->d);
  A z=ga(w->t,r,a->p);mv(z->p,w->p,wn=n>wn?wn:n);
  if(n-=wn)mv(z->p+wn,z->p,n);R z;}
 V1(sha){A z=ga(0,1,&w->r);mv(z->p,w->d,w->r);R z;}
 V1(id){R w;}V1(size){A z=ga(0,0,0);*z->p=w->r?*w->d:1;R z;}
-V pi(I i){P("%ld ",i);}V nl(){P("\n");}
+V pi(I i){P("%ld ",i);}V nl(V){P("\n");}
 V pr(A w){I r=w->r,*d=w->d,n=tr(r,d);DO(r,pi(d[i]));nl();
  if(w->t)DO(n,P("< ");pr((A)(w->p[i])))else DO(n,pi(w->p[i]));nl();}
 
 C vt[]="+{~<#,";
-A(*vd[])(A,A)={0,plus,from,find,0,rsh,cat},
+A(*vd[])(A,A)={0,plus,from,0,0,rsh,cat},
  (*vm[])(A)={0,id,size,iota,box,sha,0};
 A st[26]; I qp(I a){R  a>='a'&&a<='z';} I qv(I a){R a<'a';}
 A ex(I *e){I a=*e;
@@ -42,7 +42,7 @@ I verb(C c){I i=0;for(;vt[i];)if(vt[i++]==c)R i;R 0;}
 I *wd(C *s){I a,n=strlen(s),*e=ma(n+1);C c;
  DO(n,e[i]=(a=(I)noun(c=s[i]))?a:(a=verb(c))?a:c);e[n]=0;R e;}
 
-int main(){C s[99];I l;
+int main(V){C s[99];I l;
  while(fgets(s,sizeof(s),stdin)){
     if ((l=strlen(s))<2) continue;s[l-1]=0;
     pr(ex(wd(s)));
